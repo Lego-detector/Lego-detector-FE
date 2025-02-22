e875056cff215472d95537c9cde4aaf13f2f5856
Roshanen <numesattahip@hotmail.com>
2025-02-22 23:10:46 +0700

    feat: add md-card

--- src/modules/home/_components/FileUpload.tsx
+++ src/modules/home/_components/FileUpload.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import Image from "next/image";
 
@@ -55,39 +56,39 @@ export function FileUpload() {
   };
 
   return (
-    <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
-      <h2 className="text-xl font-bold mb-4">Upload an Image</h2>
+    <div className="md-card flex flex-col">
+      <h2 className="text-xl font-bold mb-4 text-gray-900 dark:text-white">Upload an Image</h2>
 
       <input
         type="file"
         accept="image/*"
         onChange={handleImageChange}
-        className="mb-4"
+        className="mb-4 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-300 dark:file:bg-gray-700 dark:file:text-gray-300 dark:hover:file:bg-gray-800"
       />
 
       {image && (
         <div id="image-container" className="w-64 h-64 relative">
-          <Image
-            src={image}
-            alt="Uploaded"
-            width={512} // Fixed width
-            height={512} // Fixed height
-            className="rounded-lg shadow-lg object-cover"
-          />
-        </div>
+        <Image
+          src={image}
+          alt="Uploaded"
+          layout="fill" // Fill the parent div
+          objectFit="contain" // Maintain aspect ratio
+          className="rounded-lg max-w-full max-h-full bg-gray-400" // Ensure the image doesn't exceed the div
+        />
+      </div>
       )}
 
       <button
+        type="submit"
         onClick={handleSubmit}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:bg-gray-400"
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:bg-gray-400 dark:bg-blue-500 dark:hover:bg-blue-600 dark:disabled:bg-gray-400"
         disabled={!selectedFile}
       >
         Submit
       </button>
 
-      { resMessage || <p>{resMessage}</p> }
-      { errMessage || <p className="text-red-600">{errMessage}</p> }
-
+      {resMessage && <p className="mt-2 text-green-600 dark:text-green-400">{resMessage}</p>}
+      {errMessage && <p className="mt-2 text-red-600 dark:text-red-400">{errMessage}</p>}
     </div>
   );
 };
